4dff52ae11accbc1a05f5f45be6dbb72910fc0c3
hangtiancheng <hangtiancheng@bytedance.com>
2025-09-10 17:56:53 +0800

    Initial commit

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable @typescript-eslint/no-unused-vars -->
+<script setup lang="ts">
+import NavigationBar from './components/navigation-bar.vue'
+import CopyrightInfo from '@renderer/components/copyright-info.vue'
+//! IPC 测试
+// window.electron.ipcRenderer.on('desktop-path', (_event, desktopPath: string) => {
+//   outputDir.value = desktopPath
+// })
+</script>
+
+<template>
+  <main>
+    <NavigationBar></NavigationBar> <RouterView></RouterView> <CopyrightInfo class="mt-2" />
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
